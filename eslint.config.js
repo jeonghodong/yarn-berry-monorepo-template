afe40af8ed498b86d9d64b1e321b4995c9d2eab5
hodong <fire13764@gmail.com>
2025-06-02 12:33:59 +0900

    add eslint back with proper ts sup

--- eslint.config.js
+++ eslint.config.js
@@ -1,3 +1,5 @@
+const typescriptParser = require('@typescript-eslint/parser')
+
 module.exports = [
   {
     ignores: [
@@ -12,10 +14,15 @@ module.exports = [
     ]
   },
   {
-    files: ['**/*.{js,jsx,ts,tsx}'],
+    files: ['**/*.{js,jsx}'],
     languageOptions: {
       ecmaVersion: 'latest',
       sourceType: 'module',
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true
+        }
+      },
       globals: {
         console: 'readonly',
         process: 'readonly',
@@ -25,11 +32,43 @@ module.exports = [
         global: 'readonly',
         module: 'readonly',
         require: 'readonly',
-        exports: 'readonly'
+        exports: 'readonly',
+        React: 'readonly'
       }
     },
     rules: {
       'no-unused-vars': 'warn',
+      'no-console': 'off',
+      'prefer-const': 'error',
+      'no-var': 'error'
+    }
+  },
+  {
+    files: ['**/*.{ts,tsx}'],
+    languageOptions: {
+      parser: typescriptParser,
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true
+        }
+      },
+      globals: {
+        console: 'readonly',
+        process: 'readonly',
+        Buffer: 'readonly',
+        __dirname: 'readonly',
+        __filename: 'readonly',
+        global: 'readonly',
+        module: 'readonly',
+        require: 'readonly',
+        exports: 'readonly',
+        React: 'readonly'
+      }
+    },
+    rules: {
+      'no-unused-vars': 'off', // TypeScript가 처리
       'no-console': 'off',
       'prefer-const': 'error',
       'no-var': 'error'
